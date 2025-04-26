7ee4a4859600f97dafb82fb8ee890d85cae66fed
Martin Bilek <martin.bilek@gmail.com>
2025-04-26 18:31:06 +0200

    refactor stepper motor to be controlled by waveforms

--- motor.py
+++ motor.py
@@ -1,205 +1,195 @@
-import redis
-
+import pigpio
 import time
-import RPi.GPIO as GPIO
 
-import threading
-import queue
-
-
-# Redis
-r = redis.Redis(host='localhost', port=6379, db=0)
-pubsub = r.pubsub()
-pubsub.subscribe('slinky_channel')
-redis_queue = queue.Queue()
+import redis
 
 
 # Constants
-START_DELAY = 0.0006         # initial delay (motor speed)
-DIR = 20                     # Direction GPIO Pin
-STEP = 21                    # Step GPIO Pin
-ENABLE_PIN = 18              # Enable step motor GPIO Pin
-CW = 1                       # Clockwise Rotation
-CCW = 0                      # Counterclockwise Rotation
+MOTOR_STEP_PIN = 21
+MOTOR_DIR_PIN = 20
+MOTOR_ENABLE_PIN = 18
 
-ENABLE_SENSORS = True        # whether automatic speed sensors are enabled
+START_MOTOR_STEP_DELAY_SEC = 0.0005  # initial delay (motor speed)
 
-SENSOR_BOTTOM_PIN = 27       # PIN of bottom sensor
-SENSOR_TOP_PIN = 17          # PIN of top sensor
+SENSOR_BOTTOM_PIN = 27  # PIN of bottom sensor
+SENSOR_TOP_PIN = 17     # PIN of top sensor
 
-MAX_DELAY = 0.001            # maximum delay for step motor between steps
-MIN_DELAY = 0.0001           # minimum delay for step motor between steps
-DELAY_CONST_PCT = 1.05       # constant how much in percent is changed speed
 SPEED_CHANGE_INTERVAL = 1.5  # how often can be speed changed in seconds
+DELAY_CONST_PCT = 1.05       # constant how much in percent is changed speed
 
-STEP_CYCLES_COUNT = 1231     # number of motor steps needed to run one elevator step
+SHUTDOWN_DELAY = 3.0  # delay, when motor is automatically
+                      # powered off when TOP and BOTTOM sensor are without detection
 
-PUBSUB_CHECK_INTERVAL = 5.0  # to save time/CPU we only check redis pubsub messages channel once per specified time in seconds
+STAIR_MOTOR_STEPS = 1231  # how many motor steps to travel one physical step
 
-SHUTDOWN_DELAY = 3.0         # delay, when motor is automatically
-                             # powerd off when TOP and BOTTOM sensor are without signal
+def create_step_waveform(pi, step_delay_sec):
+    '''Helper function to create one repeating step waveform'''
+    step_delay_us = int(step_delay_sec * 1_000_000)
 
+    pi.wave_clear()
 
-# Variables
-is_running = True                 # indicates whether motor is running
-last_top_time = time.time()       # time, when was object indicated on top sensor
-last_bottom_time = time.time()    # time, when was object indicated on bottom sensor
-top_detected = True               # indicates whether object is detected on top sensor during 
-bottom_detected = True            # indicates whether object is detected on bottom sensor during 
-speed_changed = True              # indicates whether speed has been changed in previous cycles already
-average_delay = START_DELAY       # calculated average delay between steps of motor
-average_count = 1                 # from how much values is average delay calculated? needed to calculate average when new numbers coming
-cycles = 0                        # how many motor steps is already done
-delay = 0.0                       # delay value
+    wf = []
+    wf.append(pigpio.pulse(1<<MOTOR_STEP_PIN, 0, step_delay_us))  # STEP high
+    wf.append(pigpio.pulse(0, 1<<MOTOR_STEP_PIN, step_delay_us))  # STEP low
 
-
-def redis_worker():
-    while True:
-        # store delay value in Redis
-        r.set('motor_speed', delay)
-        r.set('motor_avg_speed', average_delay)
-
-        # read control messages comming from outside (i.e. from frontend interface)
-        msg = pubsub.get_message()
-        if msg and msg['type'] == 'message':
-            cmd = msg['data'].decode()
-            if cmd == 'toggle_motor': # toggle motor messsage received
-                toggle_motor()
-
-        # store step value in redis
-        if redis_queue.qsize() > 0 and redis_queue.get() == 'add_step':
-            _steps = int(r.get('steps') or 0)
-            _steps += 1
-            r.set('steps', _steps)
-
-        time.sleep(0.5)
+    pi.wave_add_generic(wf)
+    wave_id = pi.wave_create()
+    return wave_id
 
 
-def toggle_motor():
-    global is_running, \
-        last_top_time, \
-        last_bottom_time, \
-        top_detected, \
-        bottom_detected, \
-        average_delay, \
-        average_count
-    is_running = not is_running
-    last_top_time = time.time()
-    last_bottom_time = time.time()
-    top_detected = True
-    bottom_detected = True
-    average_delay = START_DELAY
-    average_count = 1
+def set_motor_speed(pi, step_delay_sec):
+    '''Heleper function to start motor or change motor speed'''
+    # clear any old motor waves
+    stop_motor(pi)
+
+    # enable motor - in case it was not running before
+    pi.write(MOTOR_ENABLE_PIN, 0)
+
+    print('Changing speed...')
+
+    # Create and start new wave
+    wave = create_step_waveform(pi, step_delay_sec)
+    pi.wave_send_repeat(wave)
 
 
-def speed_down_motor():
-    global delay
-    _delay = delay * DELAY_CONST_PCT
-    if _delay > MAX_DELAY:
-        _delay = MAX_DELAY
-    delay = _delay
-
-
-def speed_up_motor():
-    global delay
-    _delay = delay / DELAY_CONST_PCT
-    if _delay < MIN_DELAY:
-        _delay = MIN_DELAY
-    delay = _delay
+def stop_motor(pi):
+    '''Stop motor'''
+    pi.wave_tx_stop()  # stop old wave
+    pi.wave_clear()    # clear old wave
 
 
 def main():
-    global delay, \
-        cycles, \
-        last_top_time, \
-        last_bottom_time, \
-        top_detected, \
-        bottom_detected, \
-        speed_changed, \
-        average_delay, \
-        average_count
+    # Connect to pigpio
+    pi = pigpio.pi()
+    if not pi.connected:
+        print('Error: pigpio not connected...')
+        exit()
 
-    threading.Thread(target=redis_worker, daemon=True).start()
+    # connect to Redis
+    r = redis.Redis(host='localhost', port=6379, db=0)
+
+    # sensors detection variables
+    last_top_time = time.time()       # time, when was object indicated on top sensor
+    last_bottom_time = time.time()    # time, when was object indicated on bottom sensor
+    top_detected = True               # indicates whether object is detected on top sensor during
+    bottom_detected = True            # indicates whether object is detected on bottom sensor during
+
+    speed_changed = True              # indicates whether speed has been changed in previous cycle already
+
+    step_log_interval = 1.5
+    step_last_log_time = time.time()
 
     try:
-        # disable GPIO warnings
-        GPIO.setwarnings(False)
+        # Setup motor
+        pi.set_mode(MOTOR_DIR_PIN, pigpio.OUTPUT)     # direction PIN
+        pi.set_mode(MOTOR_STEP_PIN, pigpio.OUTPUT)    # step PIN
+        pi.set_mode(MOTOR_ENABLE_PIN, pigpio.OUTPUT)  # enable PIN
+        pi.write(MOTOR_DIR_PIN, 0)                    # set motor direction
 
-        # configure motor
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(DIR, GPIO.OUT)
-        GPIO.setup(STEP, GPIO.OUT)
-        GPIO.output(DIR, CCW)  # motor direction
+        # Setup sensors
+        pi.set_mode(SENSOR_BOTTOM_PIN, pigpio.INPUT)
+        pi.set_mode(SENSOR_TOP_PIN, pigpio.INPUT)
 
-        # SENSOR - bottom
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(SENSOR_BOTTOM_PIN, GPIO.IN)
+        step_delay_sec = START_MOTOR_STEP_DELAY_SEC
+        last_step_delay_sec = 1000
 
-        # SENSOR - top
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(SENSOR_TOP_PIN, GPIO.IN)
+        average_step_delay_sec = START_MOTOR_STEP_DELAY_SEC  # calculated average delay between steps of motor
+        average_step_delay_count = 1                         # from how much values is average delay calculated? needed to calculate average when new numbers coming
 
-        # ENABLE MOTOR PIN - default is disabled
-        GPIO.setup(18, GPIO.OUT)
-        GPIO.output(18, GPIO.HIGH)
+        is_stopped = False
+
+        motor_steps_accumulated = 0  # count how many motor steps passed
+        last_waveform_check = time.time()
 
         while True:
-            if is_running:
-                GPIO.output(18, GPIO.LOW) # motor is enabled
 
-                if cycles > STEP_CYCLES_COUNT and cycles % STEP_CYCLES_COUNT == 0:
-                    # add slinky step
-                    redis_queue.put('add_step')
+            # top sensor dection
+            if not top_detected and pi.read(SENSOR_TOP_PIN) == 0:
+                last_top_time = time.time()
+                top_detected = True
+                speed_changed = False
+            if time.time() - last_top_time > SPEED_CHANGE_INTERVAL:
+                top_detected = False # after specified time detection is considered false
 
-                cycles += 1
+            # bottom sensor dection
+            if not bottom_detected and pi.read(SENSOR_BOTTOM_PIN) == 0:
+                last_bottom_time = time.time()
+                bottom_detected = True
+                speed_changed = False
+            if time.time() - last_bottom_time > SPEED_CHANGE_INTERVAL:
+                bottom_detected = False # after specified time detection is considered false
 
-                if not top_detected and GPIO.input(SENSOR_TOP_PIN) == GPIO.LOW:
-                    last_top_time = time.time()
-                    top_detected = True
-                    speed_changed = False
-                if time.time() - last_top_time > SPEED_CHANGE_INTERVAL:
-                    top_detected = False
+            if top_detected and bottom_detected:
+                step_delay_sec = average_step_delay_sec
 
-                if not bottom_detected and GPIO.input(SENSOR_BOTTOM_PIN) == GPIO.LOW:
-                    last_bottom_time = time.time()
-                    bottom_detected = True
-                    speed_changed = False
-                if time.time() - last_bottom_time > SPEED_CHANGE_INTERVAL:
-                    bottom_detected = False
+            # only bottom detected > speed up
+            elif not top_detected and bottom_detected:
+                if not speed_changed:
+                    step_delay_sec = step_delay_sec / DELAY_CONST_PCT
 
-                if not top_detected and bottom_detected:
-                    if not speed_changed and ENABLE_SENSORS:
-                        speed_changed = True
-                        speed_up_motor()
+            # only top detected > speed down
+            elif not bottom_detected and top_detected:
+                if not speed_changed:
+                    step_delay_sec = step_delay_sec * DELAY_CONST_PCT
 
-                if not bottom_detected and top_detected:
-                    if not speed_changed and ENABLE_SENSORS:
-                        speed_changed = True
-                        speed_down_motor()
+            # change motor speed when step delay changed from previous cycle
+            if step_delay_sec != last_step_delay_sec:
+                speed_changed = True
+                last_step_delay_sec = step_delay_sec
+                set_motor_speed(pi, step_delay_sec)
 
-                if top_detected and bottom_detected:
-                    delay = average_delay
+                if is_stopped:
+                    motor_steps_accumulated = 0
+                    last_waveform_check = time.time()
 
+                is_stopped = False
+
+            if not is_stopped:
                 if not top_detected and not bottom_detected:
                     if time.time() - last_top_time > SHUTDOWN_DELAY and time.time() - last_bottom_time > SHUTDOWN_DELAY:
-                        toggle_motor()
+                        # reset speed to default to adapt speed to different slinky faster
+                        step_delay_sec = START_MOTOR_STEP_DELAY_SEC
+                        last_step_delay_sec = START_MOTOR_STEP_DELAY_SEC
+                        average_step_delay_sec = START_MOTOR_STEP_DELAY_SEC
+                        average_step_delay_count = 1
+                        top_detected = False
+                        bottom_detected = False
 
-                average_count += 1
-                average_delay = average_delay + (delay - average_delay) / average_count
+                        print('Stopping motor...')
+                        stop_motor(pi)
+                        is_stopped = True
+                        pi.write(MOTOR_ENABLE_PIN, 1)  # disable motor
 
+                # Calculate steps
+                now = time.time()
+                elapsed = now - last_waveform_check
+                # How many motor steps happened since last check
+                motor_frequency = 1 / (2 * step_delay_sec)
+                motor_steps_passed = elapsed * motor_frequency
+                motor_steps_accumulated += motor_steps_passed
+                # If accumulated motor steps reach 1231 => 1 slinky step
+                while motor_steps_accumulated >= STAIR_MOTOR_STEPS:
+                    motor_steps_accumulated -= STAIR_MOTOR_STEPS
+                    r.set('steps', int(r.get('steps') or 0) + 1)  # add one step
+                last_waveform_check = now
 
-                GPIO.output(STEP, GPIO.HIGH)
-                time.sleep(delay)
-                GPIO.output(STEP, GPIO.LOW)
-                time.sleep(delay)
-            else:
-                GPIO.output(18, GPIO.HIGH) # motor is disabled
-                time.sleep(0.5)
+                # log to redis currnet step avg time in seconds
+                if time.time() - step_last_log_time >= step_log_interval:
+                    time_per_step_sec = STAIR_MOTOR_STEPS * (2 * step_delay_sec)
+                    r.set('time_per_step_sec', time_per_step_sec)
+                    step_last_log_time = time.time()
+
+            average_step_delay_count += 1
+            average_step_delay_sec = average_step_delay_sec + (step_delay_sec - average_step_delay_sec) / average_step_delay_count
+
+            time.sleep(0.1)
 
     except KeyboardInterrupt:
         print('Stopping motor script...')
     finally:
-        GPIO.cleanup()
+        stop_motor(pi)
+        pi.write(MOTOR_ENABLE_PIN, 1)  # disable motor
+        pi.stop()
 
 
 if __name__ == '__main__':
